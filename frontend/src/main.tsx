c85aa82405cd8e187c34c04037d41203dba9ac07
autoflowng-NG <autoflowng@gmail.com>
2026-06-13 22:26:34 +0000

    Add error overlay to debug blank screen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// Force dark class on html element for AUTOFLOWNG dark-only design
 document.documentElement.classList.add("dark");
+
+window.onerror = function(msg, src, line, col, err) {
+  document.body.innerHTML = '<div style="color:red;padding:20px;font-size:16px;background:white">' +
+    '<h2>JS Error:</h2><pre>' + msg + '\n\nFile: ' + src + '\nLine: ' + line + '\n\n' + (err?.stack || '') + '</pre></div>';
+};
+
+window.onunhandledrejection = function(e) {
+  document.body.innerHTML = '<div style="color:red;padding:20px;font-size:16px;background:white">' +
+    '<h2>Promise Error:</h2><pre>' + e.reason + '</pre></div>';
+};
 
 createRoot(document.getElementById("root")!).render(<App />);
